8696ef88c3d03ebd61341170f3dacfd9239d22a5
NyagaNjiiru <nyaganjiiru@gmail.com>
2025-07-15 21:39:58 +0300

    Fixed Supabase configs

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,20 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
+import { useEffect } from 'react';
+import checkConnection from './utils/checkConnection';
 
 function App()
 {
+  useEffect(() =>
+  {
+    checkConnection();
+  }, []);
+
   return (
     <Router>
       <Routes>
+        <Route path = "/" element = { <Navigate to = "/login" /> } />
         <Route path = "/login" element = { <Login /> } />
         <Route path = "/signup" element = { <Signup /> } />
       </Routes>
---
+++ frontend/src/utils/checkConnection.js
@@ -0,0 +1,15 @@
+import supabase from "./supabaseClient";
+
+export default async function checkConnection() {
+    try {
+        const { data, error } = await supabase.auth.getSession();
+
+        if (error) {
+            console.error('Could not connect to Supabase:', error.message);
+        } else {
+            console.log('Connected to Supabase successfully');
+        }
+    } catch (e) {
+        console.error('Unexpected error while connecting to Supabase:', e.message);
+    }
+}
