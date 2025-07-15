9eb0da65d6e707f0de70728009fefb2063f92171
NyagaNjiiru <nyaganjiiru@gmail.com>
2025-07-15 20:55:43 +0300

    Fixed structure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,9 @@
-import { useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
-import checkConnection from './utils/checkConnection';
 
-function App() {
-  useEffect(() => {
-    checkConnection();
-  }, []);
-
+function App()
+{
   return (
     <Router>
       <Routes>
--- frontend/src/supabaseClient.js
+++
@@ -1,8 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = 'https://uuabrsmatqrbopwebemg.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV1YWJyc21hdHFyYm9wd2ViZW1nIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI1OTA5MDIsImV4cCI6MjA2ODE2NjkwMn0.z00L07Lq7ILA3eW6YjF1A64dHNiKa23324YaCy7IA2U'
-
-const supabase = createClient(supabaseUrl, supabaseAnonKey);
-
-export default supabase
